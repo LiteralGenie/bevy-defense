bc098ee67f752c60a8462f84af670d4028804e76
genie <nope>
2024-03-06 05:11:54 +0000

    tidy up

--- gui/src/routes/+page.svelte
+++ gui/src/routes/+page.svelte
@@ -10,6 +10,10 @@
 	let health: Readable<number>
 
 	onMount(() => {
+		// @fixme: 	Getting the wasm / canvas to work with hot-reloading is tricky
+		//         		https://github.com/bevyengine/bevy/discussions/11619
+		//       	which is why the canvas is outside any svelte components and this onMount skips re-initing Game
+		// 	     	But this also means any changes to the Rust code / Game.ts requires manually refreshing the page
 		game = window.game ?? Game.initSingleton()
 		gold = game.state.gold
 		health = game.state.health
