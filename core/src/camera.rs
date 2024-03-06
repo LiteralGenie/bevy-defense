d36b35763c397645d4ff42a2ac0121f757f43f07
genie <nope>
2024-03-06 04:52:47 +0000

    fix lighting

--- core/src/camera.rs
+++ core/src/camera.rs
@@ -4,21 +4,15 @@ use bevy::prelude::*;
 pub struct Camera;
 
 pub fn spawn_camera(mut commands: Commands) {
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 40000.0,
-            shadows_enabled: true,
-            range: 100.0,
-            radius: 100.0,
-            ..default()
-        },
-        transform: Transform::from_xyz(0.0, 30.0, 0.0),
-        ..default()
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 1500.0,
     });
 
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 20.0, 0.0).looking_at(Vec3::ZERO, Vec3::NEG_Z),
+            transform: Transform::from_xyz(0.0, 20.0, 0.0)
+                .looking_at(Vec3::ZERO, Vec3::NEG_Z),
             ..Default::default()
         },
         Camera,
