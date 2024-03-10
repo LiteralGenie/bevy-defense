0d4ff7d74c85a17ebabd2bd44142c637a7788711
genie <nope>
2024-03-10 04:20:07 +0000

    tidy up

---
+++ gui/src/lib/components/sidebar/sidebar.svelte
@@ -0,0 +1,76 @@
+<script context="module" lang="ts">
+    interface Tower {
+        name: string
+    }
+</script>
+
+<script lang="ts">
+    import { useTowerDrag } from './use-tower-drag'
+
+    const towers: Tower[] = [
+        {
+            name: '1'
+        },
+        {
+            name: '2'
+        },
+        {
+            name: '3'
+        },
+        {
+            name: '4'
+        }
+    ]
+
+    const { isDragging, handleDragStart, handleDrag, handleDragEnd } =
+        useTowerDrag()
+</script>
+
+<div class="container">
+    <div class="grid">
+        {#each towers as tower}
+            <button
+                class="cell"
+                disabled={$isDragging}
+                draggable="true"
+                on:dragstart={handleDragStart}
+                on:drag={handleDrag}
+                on:dragend={handleDragEnd}
+            >
+                {tower.name}
+            </button>
+        {/each}
+    </div>
+</div>
+
+<style lang="scss">
+    .container {
+        height: 100%;
+
+        background: black;
+    }
+
+    .grid {
+        display: grid;
+        grid-template-columns: 50% 50%;
+    }
+
+    .cell {
+        padding: 1rem;
+
+        text-align: center;
+    }
+
+    // Borders
+    .grid {
+        border: 1px solid white;
+        border-right: 0;
+        border-top: 0;
+
+        .cell {
+            border: 1px solid white;
+            border-left: 0;
+            border-bottom: 0;
+        }
+    }
+</style>
--- gui/src/routes/+page.svelte
+++ gui/src/routes/+page.svelte
@@ -1,84 +1,84 @@
 <script lang="ts">
-	import Info from '$lib/components/info.svelte'
-	import Sidebar from '$lib/components/sidebar.svelte'
-	import { Game } from '$lib/game/game'
-	import { onMount } from 'svelte'
-	import { type Readable } from 'svelte/store'
+    import Info from '$lib/components/info.svelte'
+    import Sidebar from '$lib/components/sidebar/sidebar.svelte'
+    import { Game } from '$lib/game/game'
+    import { onMount } from 'svelte'
+    import { type Readable } from 'svelte/store'
 
-	let game: Game
-	let gold: Readable<number>
-	let health: Readable<number>
+    let game: Game
+    let gold: Readable<number>
+    let health: Readable<number>
 
-	onMount(() => {
-		// @fixme: 	Getting the wasm / canvas to work with hot-reloading is tricky
-		//         		https://github.com/bevyengine/bevy/discussions/11619
-		//       	which is why the canvas is outside any svelte components and this onMount skips re-initing Game
-		// 	     	But this also means any changes to the Rust code / Game.ts requires manually refreshing the page
-		game = window.game ?? Game.initSingleton()
-		gold = game.state.gold
-		health = game.state.health
-	})
+    onMount(() => {
+        // @fixme: 	Getting the wasm / canvas to work with hot-reloading is tricky
+        //         		https://github.com/bevyengine/bevy/discussions/11619
+        //       	which is why the canvas is outside any svelte components and this onMount skips re-initing Game
+        // 	     	But this also means any changes to the Rust code / Game.ts requires manually refreshing the page
+        game = window.game ?? Game.initSingleton()
+        gold = game.state.gold
+        health = game.state.health
+    })
 </script>
 
 {#if game}
-	<div class="container">
-		<div class="info">
-			<Info />
-		</div>
+    <div class="container">
+        <div class="info">
+            <Info />
+        </div>
 
-		<div class="sidebar">
-			<Sidebar />
-		</div>
-	</div>
+        <div class="sidebar">
+            <Sidebar />
+        </div>
+    </div>
 {/if}
 
 <style lang="scss">
-	// Full-screen the game canvas
-	:global(body) {
-		margin: 0;
-		overflow: hidden;
-	}
-	:global(.game-canvas-container) {
-		height: 100vh;
-		width: 100vw;
+    // Full-screen the game canvas
+    :global(body) {
+        margin: 0;
+        overflow: hidden;
+    }
+    :global(.game-canvas-container) {
+        height: 100vh;
+        width: 100vw;
 
-		:global(#game-canvas) {
-			height: 100% !important;
-			width: 100% !important;
-		}
-	}
+        :global(#game-canvas) {
+            height: 100% !important;
+            width: 100% !important;
+        }
+    }
 
-	// Make the gui an overlay
-	.container {
-		position: absolute;
-		height: 100%;
-		width: 100%;
-	}
+    // Make the gui an overlay
+    .container {
+        position: absolute;
+        height: 100%;
+        width: 100%;
+    }
 
-	// For drag-and-drop purposes, ignore most of the gui overlay
-	// when calculating the drop target (document.elementFromPoint())
-	.container {
-		pointer-events: none;
+    // For drag-and-drop purposes, ignore most of the gui overlay
+    // when calculating the drop target (document.elementFromPoint())
+    .container {
+        pointer-events: none;
 
-		.sidebar {
-			pointer-events: all;
-		}
-	}
+        .sidebar {
+            pointer-events: all;
+        }
+    }
 
-	.container {
-		display: grid;
-		grid-template-columns: 1fr max(200px, 20vw);
+    .container {
+        display: grid;
+        grid-template-columns: 1fr max(200px, 20vw);
 
-		color: white;
+        color: white;
 
-		.info {
-			height: max-content;
-			width: max-content;
-			justify-self: flex-end;
-		}
+        .info {
+            height: max-content;
+            width: max-content;
+            justify-self: flex-end;
+        }
 
-		.sidebar {
-			height: 100%;
-		}
-	}
+        .sidebar {
+            height: 100%;
+        }
+    }
 </style>
